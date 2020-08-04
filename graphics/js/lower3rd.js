a08ff46ac72b38259fc225d4087641735f5041bf
rolandviper <56328676+rolandviper@users.noreply.github.com>
2020-08-04 16:10:07 +0800

    updated lower3rd and added schedule
    will need to look into better lower3rd animation

--- graphics/js/lower3rd.js
+++ graphics/js/lower3rd.js
@@ -4,12 +4,28 @@ const tl = gsap.timeline();
 const title = document.getElementById('title');
 const sub = document.getElementById('sub');
 
-const black = document.querySelector('.black');
+const black = document.querySelectorAll('.black');
+const orange = document.querySelectorAll('.orange');
 
 nodecg.listenFor('updatel3', (data) => {
 	title.innerHTML = data.title;
 	sub.innerHTML = data.sub;
 
-	tl.to(black, 0.6, { ease: 'power3.in', width: '50%' }, '+=0.4');
-	tl.from('#title', { duration: 0.2, opacity: 0 });
+	tl.to(black, { ease: 'power3.in', width: '100%', duration: 0.5 });
+
+	tl.to(orange, { ease: 'power2.in', width: '100%', duration: 0.2 }, '-=0.2');
+	tl.from([ '#title', '#sub' ], { duration: 0.2, opacity: 0 });
+	tl.to(orange, { ease: 'power3.out', width: '0', duration: 0.5 });
+	tl.to(black, { ease: 'power1.out', width: '0', duration: 0.2 });
+
+	tl.to(black, { ease: 'power3.out', width: '100%', duration: 0.5 }, '+=5');
+	tl.to([ '#title', '#sub' ], { duration: 0.2, opacity: 0 });
+	tl.to(black, { ease: 'power3.out', width: '0', duration: 0.5 });
+
+	tl.call(() => {
+		title.innerHTML = '';
+		sub.innerHTML = '';
+	});
+
+	tl.set([ '#title', '#sub' ], { width: '', opacity: '' });
 });
---
+++ graphics/js/schedule.js
@@ -0,0 +1,52 @@
+import { gsap } from '../../node_modules/gsap/index.js';
+
+const nameplateEl = document.getElementById('nameplate');
+const titleEl = document.getElementById('title');
+
+const event1 = document.getElementById('event1');
+const time1 = document.getElementById('time1');
+const event2 = document.getElementById('event2');
+const time2 = document.getElementById('time2');
+const event3 = document.getElementById('event3');
+const time3 = document.getElementById('time3');
+const event4 = document.getElementById('event4');
+const time4 = document.getElementById('time4');
+
+const events = document.querySelectorAll('.event');
+const text = document.querySelectorAll('.text');
+
+nodecg.listenFor('showSchedule', (data) => {
+	event1.innerHTML = data.event1;
+	time1.innerHTML = data.time1;
+	event2.innerHTML = data.event2;
+	time2.innerHTML = data.time2;
+	event3.innerHTML = data.event3;
+	time3.innerHTML = data.time3;
+	event4.innerHTML = data.event4;
+	time4.innerHTML = data.time4;
+
+	const tl = gsap.timeline();
+
+	tl.to(nameplateEl, { duration: 0.5, autoAlpha: 1 });
+
+	tl.from(text, { duration: 1, width: 0, stagger: 0.5, autoAlpha: 0, backgroundColor: 'black' });
+
+	tl.to(text, { duration: 0.5, width: 0, stagger: -0.2, autoAlpha: 0, backgroundColor: 'yellow' }, '+=5');
+
+	tl.call(() => {
+		event1.innerHTML = '';
+		time1.innerHTML = '';
+		event2.innerHTML = '';
+		time2.innerHTML = '';
+		event3.innerHTML = '';
+		time3.innerHTML = '';
+		event4.innerHTML = '';
+		time4.innerHTML = '';
+	});
+
+	tl.set([ nameplateEl, event1, time1, event2, time2, event3, time3, event4, time4, text ], {
+		width: '',
+		opacity: '',
+		backgroundColor: ''
+	});
+});
